9c0158b062e4677297506421b8403c58c299cafd
Emehado <emehado@gmail.com>
2021-01-19 13:51:56 +0000

    set price limit for listing upload

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -18,7 +18,7 @@ import UserContext from "../auth/userContext";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
-  price: Yup.number().required().min(1).max(100000).label("Price"),
+  price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
   images: Yup.array().min(1, "Please select atleast one image"),
